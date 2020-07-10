a6c21b99cb07debf4fbcd6bb4b2528dbe8dc2ae2
Velythyl <charlie.gauthier@umontreal.ca>
2020-07-10 17:27:54 -0400

    added lotsa dockers as per Anthony C.'s suggestion

--- docker_launcher.py
+++ docker_launcher.py
@@ -68,7 +68,10 @@ parser = parser.parse_args()
 dockers_to_run = [   "-v /var/run/docker.sock:/var/run/docker.sock -v /var/local:/var/local duckietown/dt-device-loader:daffy-arm32v7",
                      "duckietown/dt-duckiebot-interface:daffy-arm32v7",
                      "duckietown/dt-car-interface:daffy-arm32v7",
-                     "--gpus all duckietown/dt-core:daffy-arm32v7"
+                     "--gpus all duckietown/dt-core:daffy-arm32v7",
+                     "duckietown/dt-device-online:daffy-arm32v7",
+                     "duckietown/dt-rosbridge-websocket:daffy-arm32v7",
+                     "duckietown/rpi-simple-server:master18"
                  ] + parser.dockers
 
 
