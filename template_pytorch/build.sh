cddf28221bea742dcdefc2c84516a23a613a771c
Velythyl <charlie.gauthier@umontreal.ca>
2020-07-21 14:32:02 -0400

    base succesfully built; trying pytorch now

--- template_pytorch/build.sh
+++ template_pytorch/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp -r /usr/lib/aarch64-linux-gnu ./jetson_libs
+mount -t bind /usr/lib/aarch64-linux-gnu ./jetson_libs
 docker build -t template-pytorch .
 docker run --device=/dev/nvhost-ctrl --device=/dev/nvhost-ctrl-gpu --device=/dev/nvhost-prof-gpu --device=/dev/nvmap --device=/dev/nvhost-gpu --device=/dev/nvhost-as-gpu -v /usr/lib/aarch64-linux-gnu/tegra:/usr/lib/aarch64-linux-gnu/tegra template-pytorch
+umount ./jetson_libs
 # TODO docker commit
