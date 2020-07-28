4dc8c8aad49db16b85f58564f1b0298fed3ece5b
Velythyl <charlie.gauthier@umontreal.ca>
2020-07-28 12:51:14 -0400

    opencv directly in base

--- opencv-builder-qemu/build_opencv.sh
+++ opencv-builder-qemu/build_opencv.sh
@@ -3,3 +3,5 @@
 mkdir build || rm -rf build && mkdir build
 docker build -t opencv-build-jetson .
 docker run -v $PWD/build:/build opencv-build-jetson
+mkdir ../base/opencv_build || rm -rf ../base/opencv_build && mkdir ../base/opencv_build
+cp -r ./build/* ../base/opencv_build
