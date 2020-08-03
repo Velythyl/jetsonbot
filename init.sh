02a671214ecf855207728be04bceb367ee8b4a75
Velythyl <charlie.gauthier@umontreal.ca>
2020-08-03 13:55:56 -0400

    setup.sh going along

--- init.sh
+++ init.sh
@@ -67,20 +67,19 @@ true
 
 #docker_stuff
 
-function setup_camera_stream() {
 cd /usr/src/linux-headers-4.9.140-tegra-ubuntu18.04_aarch64/kernel-4.9
 mkdir v4l2loopback
 git clone https://github.com/umlaeute/v4l2loopback.git v4l2loopback
 cd v4l2loopback
 make
 make install
+depmod -a
 apt-get install -y v4l2loopback-dkms v4l2loopback-utils
 modprobe v4l2loopback devices=1 video_nr=2 exclusive_caps=1
 echo options v4l2loopback devices=1 video_nr=2 exclusive_caps=1 > /etc/modprobe.d/v4l2loopback.conf
 echo v4l2loopback > /etc/modules
 update-initramfs -u
-echo "${FUNCNAME[0]} has run successfully"
+echo "PIPELINE SETUP has run successfully"
 true
-}
 
 setup_camera_stream
