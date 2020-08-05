4104e905dc3c0d2d5436fbfde3eddff9d3e686c3
Velythyl <charlie.gauthier@umontreal.ca>
2020-08-05 16:46:28 -0400

    setup.sh going along

--- docker_launcher.py
+++ docker_launcher.py
@@ -58,6 +58,7 @@ parser.add_argument("-d", "--dockers", default=[], type=list_of_str)
 parser = parser.parse_args()
 
 dockers_to_run = [   "-v /var/run/docker.sock:/var/run/docker.sock -v /var/local:/var/local duckietown/dt-device-loader:daffy-arm32v7",
+                     "duckietown/dt-ros-commons:daffy",
                      "velythyl/dt-duckiebot-interface:daffy-amd64",
                      "duckietown/dt-car-interface:daffy-arm32v7",
                      "--gpus all duckietown/dt-core:daffy-arm32v7",
